aa031ad23dfb3c1534cd01b6cd2e581faacbaeb1
Jay200123 <kenken.ayonon@gmail.com>
2022-11-25 18:12:24 +0800

    5th commit

--- resources/views/partials/test_header.blade.php
+++ resources/views/partials/test_header.blade.php
@@ -6,9 +6,11 @@
    }
 
  .navbar { 
-  background: grey;
+  /* background: linear-gradient(to bottom, white 0%, black 100%); */
+  background-color:white;
   padding: 15px;
-  border: solid;
+  border-radius:5px;
+  border:1px solid grey;
   /* box-shadow: 15px 10px; */
  }
 
@@ -25,18 +27,19 @@
   justify-content: flex-end;
  }
 
- .divider{
-  display:inline-block;
- }
-
- li{
-  font-family: sans-serif;
-  display:inline-block;
- }
-
  a{
   color:White;
   text-decoration:none;
+ }
+
+ p{
+  color:black;
+  font-family: sans-serif;
+  font-size: 15px;
+ }
+
+ li{
+  margin-right:25px
  }
 
 </style>
@@ -44,24 +47,23 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link href="css/bootstrap.min.css" rel="stylesheet">
 <nav class="navbar navbar-expand-md"> 
-        <a class="navbar-brand" href="#">FootWear PH</a> 
+      <a class="navbar-brand" href="#"> <p><strong>FootWear PH</p></strong></a> 
         <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
          <span class="navbar-toggler-icon"></span> 
         </button> 
         <div class="collapse navbar-collapse" id="main-navigation"> 
         <ul class="navbar-nav"> 
-        <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-user" aria-hidden="true"></i>Customer</a> </li>
-         <li class="nav-item"> <a class="nav-link" href="index_contact.html"><i class="fa fa-user-md" aria-hidden="true"></i>Employee</a> </li> 
-        <li class="nav-item"> <a class="nav-link" href="#"><i class="fa fa-phone" aria-hidden="true"></i>Services</a> </li>
-        <li class="nav-item"> <a class="nav-link" href="{{route('product.index')}}"><i class="fa fa-product-hunt"  aria-hidden="true"></i>Products</a> </li>
+        <li class="nav-item"><a class="nav-link" href="#"><p><strong><i class="fa fa-user" aria-hidden="true"></i>Customer</strong></p></a> </li>
+         <li class="nav-item"> <a class="nav-link" href="index_contact.html"><p><strong><i class="fa fa-user-md" aria-hidden="true"></i>Employee</strong></p></a> </li> 
+        <li class="nav-item"> <a class="nav-link" href="#"><p><strong><i class="fa fa-phone" aria-hidden="true"></i>Services</strong></p></a> </li>
+        <li class="nav-item"> <a class="nav-link" href="{{route('product.index')}}"><p><strong><i class="fa fa-product-hunt"  aria-hidden="true"></i>Products</strong></p></a></li>
         </ul> 
         
 
         <ul class="nav navbar-nav navbar-right">
         @if (Auth::check())
-              <li><i class="fa fa-user" aria-hidden="true"><a  href="{{ route('employee.profile') }}">Employee Profile</a></i></li>
-              <li role="separator" class="divider"></li>
-              <li><i class="fa fa-sign-out" aria-hidden="true"><a  href="{{ route('user.logout') }}">Logout</a></i></li>
+              <li><i class="fa fa-user-circle" aria-hidden="true"><a  href="{{ route('employee.profile') }}"><p><strong>User Profile</strong></p></a></i></li>
+              <li><i class="fa fa-power-off" aria-hidden="true"><a  href="{{ route('user.logout') }}"><p><strong>Logout</strong></p></a></i></li>
               @else
             <li><a href="index_signup.html"><span class="glyphicon glyphicon-user"><i class="fa fa-user-plus" aria-hidden="true"></i></span> Sign Up</a></li>
             <li><a href="index_signin.html"><span class="glyphicon glyphicon-log-in"><i class="fa fa-sign-in" aria-hidden="true"></i></span> Login</a></li>
